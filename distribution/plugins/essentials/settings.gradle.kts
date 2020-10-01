55357673d824b60d1f258d64a0285cd50398d472
Jendrik Johannes <jendrik@gradle.com>
2020-10-01 16:57:40 +0200

    Expand settings code as included build settings plugins do not yet work
    https://github.com/gradle/gradle/issues/14562

--- distribution/plugins/essentials/settings.gradle.kts
+++ distribution/plugins/essentials/settings.gradle.kts
@@ -1,6 +1,24 @@
-plugins {
-    id("gradlebuild.settings")
+// repositories for plugins
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven {
+            setUrl("https://repo.gradle.org/gradle/libs-releases")
+        }
+        maven {
+            setUrl("https://repo.gradle.org/gradle/enterprise-libs-release-candidates-local")
+        }
+    }
 }
+
+// include all folders with a build script as subproject
+settingsDir.listFiles()!!.filter { it.hasBuildScript() }.forEach { subproject ->
+    include(subproject.name)
+}
+
+fun File.hasBuildScript() =
+    File(this, "build.gradle.kts").exists() || File(this, "build.gradle").exists()
+
 
 includeBuild("../../../build-src")
 
